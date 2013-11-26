72a2bc522d47cce51010736b524b14ed85fc98d1
Nakia <nakiaaresz@gmail.com>
2013-11-26 18:21:18 +0100

    Ellenorzes 1

--- Src/Program.cs
+++ Src/Program.cs
@@ -15,5 +15,22 @@ namespace beadando
 
             Console.ReadKey();
         }
+
+        static bool Ellenorzes(Bejegyzes[] bejegy)
+        {
+            //nincs két különböző bejegyzés, amelyekben mind az első, mind a második anyag megegyezne
+            Bejegyzes seged;
+            for (int i = 0; i < bejegy.Length; i++)
+            {
+                seged = bejegy[i];
+                for (int j = i + 1; j < bejegy.Length; j++)
+                {
+                    if (seged.Kezdo_anyag == bejegy[j].Kezdo_anyag && seged.Veg_anyag == bejegy[j].Veg_anyag)
+                        return false;
+                }
+                return true;
+            }
+            return false;
+        }
     }
 }
